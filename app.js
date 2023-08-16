73b1d620b3dcd8e2e3a873840d0e0bca76c49e5a
Ekaterina Danilova <ekat.sokolova2010@yandex.ru>
2023-08-16 14:29:48 +0300

    helmet and limiter

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const router = require('./routes/index');
 const handlerErrors = require('./middlewares/handlerErrors');
 
@@ -12,6 +14,14 @@ mongoose.connect(MESTO_DB, {
   useUnifiedTopology: true,
 });
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+app.use(limiter);
+
+app.use(helmet());
 app.use(express.json());
 app.use('/', router);
 
